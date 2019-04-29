5c5012b7e3154ce592e091fde79766426ddd5b8c
isaac0000 <50117318+isaac0000@users.noreply.github.com>
2019-04-29 15:27:43 -0300

    Add files via upload

---
+++ jogo002/joguin (3).js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1556562236,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/starshipcontroller-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/bulletcontroller-sheet0.png",
+		"images/arrowupbutton-sheet0.png",
+		"images/arrowdownbutton-sheet0.png",
+		"images/arrowrightbutton-sheet0.png",
+		"images/arrowleftbutton-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
